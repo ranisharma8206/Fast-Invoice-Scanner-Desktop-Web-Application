f87fa460e91a5007b09ab6a659785e5477483546
ranisharma8206 <ranisharma8206@gmail.com>
2021-02-24 19:02:20 +0530

    Updated IP addr and working for less than 1b images.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Main_container from './components/Main_container.js'
 import { ViewerInitialScreen } from './components/ViewerInitialScreen.js';
 import { io } from "socket.io-client";
 
-const socket = io("http://127.0.0.1:5000/");
+const socket = io("http://192.168.43.88:5000/");
 
 
 function App() {
